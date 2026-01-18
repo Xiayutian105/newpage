8d6b40f2e6018f72bd09b6bae9538f0d367a0fd4
Xiayutian105 <53372921+Xiayutian105@users.noreply.github.com>
2026-01-18 20:43:46 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,13 @@
+// 网址数据
+const websites = [
+  { title: "云粘贴", url: "https://webnote.cc/", icon: "fa fa-clipboard" }, // 剪贴板专用图标，直接关联剪贴板功能
+  { title: "抠抠图", url: "https://www.koukoutu.com/?ref=aishenqi.net/", icon: "fa fa-image" }, // 图片图标，代表图片编辑功能
+  { title: "GitHub", url: "https://github.com", icon: "fab fa-github" }, // GitHub品牌图标，使用Font Awesome 6语法
+  { title: "视频解析", url: "https://www.v2ob.com/", icon: "fa fa-video" }, // 视频图标，代表视频解析功能
+  { title: "微信解析", url: "https://greenvideo.cc/gzh/", icon: "fab fa-weixin" }, // 微信品牌图标，使用Font Awesome 6语法
+  { title: "无损放大", url: "https://bigjpg.com/", icon: "fa fa-search-plus" }, // 放大镜加号图标，代表放大功能
+  { title: "果核剥壳", url: "https://www.ghxi.com/", icon: "fa fa-wrench" }, // 工具图标，代表科技工具网站
+  { title: "软件下载", url: "http://www.zhanshaoyi.com/", icon: "fa fa-download" }, // 下载图标，代表下载功能
+  { title: "阿酷下载", url: "https://www.coolexe.com/", icon: "fa fa-download" }, // 下载图标，代表下载功能
+  { title: "网址导航", url: "https://ppmc.club/navhub/", icon: "fa fa-download" } // 网址导航，代表导航功能
+]
